9919f928ff46b288fc4c55147fc068b896782645
Sandra Marcela Llanos Alvarez <sandra.llanos@sophos.col.com>
2019-10-06 20:30:19 -0500

    link heros

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../../services/hero.service';
+
 
 @Component({
   selector: 'app-hero',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
+  hero: any = {};
+  casa: string;
+  casaHero: boolean;
+  constructor( private _activatedRoute:ActivatedRoute,
+              private _heroService:HeroService
+    ) {
 
-  constructor() { }
+   this._activatedRoute.params.subscribe(params => {
+     console.log(params['id'])
+     this.hero = this._heroService.getHero(params['id']);
+     console.log(this.hero);
+    })
+   }
+
+   MostrarCasa(){
+     this.casa = this.hero.casa;
+      if (this.casa == "Marvel"){
+        this.casaHero = true;
+      }else{
+        this.casaHero = false;
+      }
+   }
 
   ngOnInit() {
+    this.MostrarCasa();
   }
 
 }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService, Heroe } from 'src/app/services/hero.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +10,18 @@ import { HeroService, Heroe } from 'src/app/services/hero.service';
 export class HeroesComponent implements OnInit {
   heroes:Heroe[] = [];
 
-  constructor(private _serviceHero:HeroService) {
+  constructor( private _serviceHero:HeroService,
+               private _router:Router
+              ) {
   }
   
   ngOnInit() {
     this.heroes=this._serviceHero.getHeroes();
-    console.log(this.heroes);
+    // console.log(this.heroes);
+  }
+  verHeroes(idHero){
+    // console.log(idHero);
+    this._router.navigate(['/hero', idHero]);
   }
 
 }
